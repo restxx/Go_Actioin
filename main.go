831c24de5a20eb4652fd4cbdff3990eae0e19d58
dongguanghua <doc007@163.com>
2023-11-08 12:35:44 +0800

    Gin

--- main.go
+++ main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
-	"log"
-	"os"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+// func main() {
 
-	log.Printf("hello world")
-}
-func init() {
-	file := "./" + "message" + ".txt"
-	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
-	if err != nil {
-		panic(err)
-	}
-	log.SetOutput(logFile) // 将文件设置为log输出的文件
-	log.SetPrefix("[qSkipTool]")
-	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
-	return
+// 	log.Printf("hello world")
+// }
+// func init() {
+// 	file := "./" + "message" + ".txt"
+// 	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	log.SetOutput(logFile) // 将文件设置为log输出的文件
+// 	log.SetPrefix("[qSkipTool]")
+// 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+// 	return
+// }
+
+// 实现一个以gin为框架的http服务demo
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+	r.Run("0.0.0.0:8888") // listen and serve on 0.0.0.0:8080
 }
